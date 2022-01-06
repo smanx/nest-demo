5ec038cd53448a8cf39bc095fc64ed03043fcd61
zhaochuan <email>
2022-01-06 19:22:41 +0800

    save

--- bin/cp.js
+++ bin/cp.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-cp('bin/vercel.json', 'dist/vercel.json')
-cp('bin/package.json', 'dist/package.json')
+// cp('bin/vercel.json', 'dist/vercel.json')
+// cp('bin/package.json', 'dist/package.json')
 
 function cp(frome, to) {
 	fs.readFile(frome, function (err, data) {
