861aeaf05cc0d44c0e15660e102e627f235680a0
smanx <497704568@qq.com>
2022-01-11 10:10:55 +0000

    Deploying to dist from @ smanx/nest-demo@ef3cc1d0fb92b663fe3e4917ce14bdcb3f421394 🚀

--- common/transform.interceptor.js
+++ common/transform.interceptor.js
@@ -11,13 +11,11 @@ const common_1 = require("@nestjs/common");
 const rxjs_1 = require("rxjs");
 const operators_1 = require("rxjs/operators");
 const operators_2 = require("rxjs/operators");
-const model_log_1 = require("../model/model.log");
 const store_1 = require("./store");
 let TransformInterceptor = class TransformInterceptor {
     intercept(context, next) {
         return next.handle().pipe((0, operators_2.catchError)(err => (0, rxjs_1.throwError)(new common_1.BadGatewayException()))).pipe((0, operators_1.map)(data => {
             let [req, res, next] = context['args'];
-            (0, model_log_1.ModelLogAdd)(data, req, res);
             let id = `id_${new Date().getTime()}_${parseInt((Math.random() * 10000).toString())}`;
             store_1.default.LOG.addRes({ data, req, res, id });
             return data;
